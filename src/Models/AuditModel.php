da48a1ea6bfb00f3d8308c0c05456213ea9f197b
Paresh Maheshwari <kumarpareshmaheshwari@gmail.com>
2026-07-09 10:07:54 +0000

    Fix Pint style issues in AuditModel

--- src/Models/AuditModel.php
+++ src/Models/AuditModel.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Paresh27\ModelAuditorLaravel\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 /**
  * @property int $id
  * @property string $subject
  * @property array<string, array{old: mixed, new: mixed}> $changes
- * @property \Illuminate\Support\Carbon $occurred_at
+ * @property Carbon $occurred_at
  */
 class AuditModel extends Model
 {
